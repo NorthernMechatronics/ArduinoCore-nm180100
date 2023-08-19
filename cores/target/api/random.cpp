69010cd4b435560d00c5699bfd83f1b139045407
Joshua Wong <joshua@northernmechatronics.com>
2023-08-19 16:22:40 -0400

    Implemented random functions

---
+++ cores/target/api/random.cpp
@@ -0,0 +1,84 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2023, Northern Mechatronics, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+
+#include <am_mcu_apollo.h>
+#include <am_util.h>
+
+#include "ArduinoAPI.h"
+
+static long entropy_retrieve()
+{
+    return rand();
+}
+
+void randomSeed(unsigned long seed)
+{
+    if (seed != 0)
+    {
+        srand(seed);
+    }
+    else
+    {
+    }
+}
+
+long random(long r_min, long r_max)
+{
+    if (r_min >= r_max)
+    {
+        return r_min;
+    }
+
+    long delta = r_max - r_min;
+    long val = (entropy_retrieve() % delta) + r_min;
+
+    return val;
+}
+
+long random(long r_max)
+{
+    if (r_max == 0)
+    {
+        return 0;
+    }
+    else if (r_max < 0)
+    {
+        return random(0, -r_max);
+    }
+    else
+    {
+        long val = entropy_retrieve();
+        return val % r_max;
+    }
+}
