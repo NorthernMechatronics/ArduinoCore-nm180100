1a4a9a6c943ceb6b0233ea09bc499074279ca245
Joshua Wong <joshua@northernmechatronics.com>
2023-08-26 15:33:59 -0400

    Added missing files

--- extras/release_package.bat
+++ extras/release_package.bat
@@ -6,28 +6,32 @@ IF "%~3" == "" GOTO error
 
 SET "DESTINATION=staging/ArduinoCore-%~1"
 robocopy cores "%DESTINATION%/cores" *.h *.a /e
-robocopy "libraries" "%DESTINATION%/libraries" *.h *.a /e
-robocopy "variants/%~1" "%DESTINATION%/%~1" *.h *.a /e
+robocopy "libraries" "%DESTINATION%/libraries" library.properties *.h *.a /e
+robocopy "variants/%~1" "%DESTINATION%/variants/%~1" AMA3B1KK.ld loadscript *.h *.a /e
+copy "variants\stub.c" "%DESTINATION%\variants"
 
 copy boards.txt "%DESTINATION%"
 copy platform.txt "%DESTINATION%"
 copy programmers.txt "%DESTINATION%"
 
-sed -i "s/\[VARIANT\]/%~1/g" "%DESTINATION%/platform.txt"
 sed -i "s/\[VERSION\]/%~2/g" "%DESTINATION%/platform.txt"
 
 SET "FILENAME=%DESTINATION%-%~2.tar.bz2"
-SET "VERSION=%~2"
-tar --exclude='*.git' --exclude='*.github' -cjhf "%FILENAME%" "%DESTINATION%"
-for /f "delims= " %%i in ('sha256sum %FILENAME%') do set CHECKSUM=%%i
-for /f "delims= " %%i in ('wc -c %FILENAME%') do set SIZE=%%i
 for /f "delims= " %%i in ('basename %FILENAME%') do set BASENAME=%%i
+for /f "delims= " %%i in ('basename %DESTINATION%') do set BASEFOLDER=%%i
+SET "VERSION=%~2"
+
+cd staging
+tar --exclude='*.git' --exclude='*.github' -cjhf "%BASENAME%" "%BASEFOLDER%"
+for /f "delims= " %%i in ('sha256sum %BASENAME%') do set CHECKSUM=%%i
+for /f "delims= " %%i in ('wc -c %BASENAME%') do set SIZE=%%i
+cd ..
 
 SET "JSON_FILE=staging/package_%~1_%~2_index.json"
 copy extras\package_index_template.json "%JSON_FILE%"
 sed -i "s/%%%%VARIANT%%%%/%~1/g" "%JSON_FILE%"
 sed -i "s/%%%%VERSION%%%%/%~2/g" "%JSON_FILE%"
-sed -i "s/%%%%URL%%%%/%~3/g" "%JSON_FILE%"
+sed -i "s#%%%%URL%%%%#%~3#g" "%JSON_FILE%"
 sed -i "s/%%%%ARCHIVENAME%%%%/%BASENAME%/g" "%JSON_FILE%"
 sed -i "s/%%%%CHECKSUM%%%%/%CHECKSUM%/g" "%JSON_FILE%"
 sed -i "s/%%%%SIZE%%%%/%SIZE%/g" "%JSON_FILE%"
