60e48e0f05b6c583b4c0e7297a67fe020e02e09a
Joshua Wong <joshua@northernmechatronics.com>
2023-09-23 12:09:50 -0400

    implemented pulseInLong

--- cores/target/api/pulse.cpp
+++ cores/target/api/pulse.cpp
@@ -32,15 +32,21 @@
 
 #include <stdlib.h>
 #include <am_mcu_apollo.h>
+
+#include <FreeRTOS.h>
+#include <task.h>
+
 #include "ArduinoAPI.h"
 
-typedef struct pulse_context_s
-{
-    uint8_t state;
-    uint8_t phase;
-} pulse_context_t;
-
 extern "C" uint32_t clockFrequency(void);
+
+static void pulseCallback(void *parameter)
+{
+    BaseType_t context_switch;
+    TaskHandle_t handle = (TaskHandle_t)parameter;
+    xTaskNotifyFromISR(handle, 0, eNoAction, &context_switch);
+    portYIELD_FROM_ISR(context_switch);
+}
 
 unsigned long pulseIn(pin_size_t pin, uint8_t state, unsigned long timeout)
 {
@@ -60,6 +66,7 @@ unsigned long pulseIn(pin_size_t pin, uint8_t state, unsigned long timeout)
         tick_current = am_hal_stimer_counter_get();
         if (tick_current > tick_end)
         {
+            am_hal_gpio_pinconfig(pin, g_AM_HAL_GPIO_DISABLE);
             return 0;
         }
     }
@@ -69,6 +76,7 @@ unsigned long pulseIn(pin_size_t pin, uint8_t state, unsigned long timeout)
         tick_current = am_hal_stimer_counter_get();
         if (tick_current > tick_end)
         {
+            am_hal_gpio_pinconfig(pin, g_AM_HAL_GPIO_DISABLE);
             return 0;
         }
     }
@@ -80,11 +88,12 @@ unsigned long pulseIn(pin_size_t pin, uint8_t state, unsigned long timeout)
         tick_current = am_hal_stimer_counter_get();
         if (tick_current > tick_end)
         {
+            am_hal_gpio_pinconfig(pin, g_AM_HAL_GPIO_DISABLE);
             return 0;
         }
     }
     tick_stop = am_hal_stimer_counter_get();
-    am_hal_gpio_fastgpio_disable(pin);
+    am_hal_gpio_pinconfig(pin, g_AM_HAL_GPIO_DISABLE);
 
     duration_us = (tick_stop - tick_start) * 1000000 / cpu_frequency;
 
@@ -93,6 +102,65 @@ unsigned long pulseIn(pin_size_t pin, uint8_t state, unsigned long timeout)
 
 unsigned long pulseInLong(pin_size_t pin, uint8_t state, unsigned long timeout)
 {
+    TaskHandle_t current_task;
     unsigned long duration_us = 0;
+    uint32_t tick_start, tick_stop, tick_current;
+    uint32_t tick_timeout, tick_end, cpu_frequency;
+
+    am_hal_gpio_pincfg_t gpio_pincfg = g_AM_HAL_GPIO_INPUT;
+    AM_HAL_GPIO_MASKCREATE(sGpioMask);
+    AM_HAL_GPIO_MASKBIT(psGpioMask, pin);
+
+    cpu_frequency = clockFrequency();
+    tick_timeout = (timeout / 1000000.0) * cpu_frequency;
+    current_task = xTaskGetCurrentTaskHandle();
+
+    gpio_pincfg.eIntDir = AM_HAL_GPIO_PIN_INTDIR_BOTH;
+    am_hal_gpio_pinconfig(pin, gpio_pincfg);
+    am_hal_gpio_interrupt_register_adv(pin, pulseCallback, current_task);
+    am_hal_gpio_interrupt_clear(psGpioMask);
+    am_hal_gpio_interrupt_enable(psGpioMask);
+
+    if (am_hal_gpio_input_read(pin) == state)
+    {
+        if (xTaskNotifyWait(0, 1, NULL, pdMS_TO_TICKS(timeout / 1000)) == pdFALSE)
+        {
+            am_hal_gpio_interrupt_disable(psGpioMask);
+            am_hal_gpio_interrupt_clear(psGpioMask);
+            am_hal_gpio_pinconfig(pin, g_AM_HAL_GPIO_DISABLE);
+            return 0;
+        }
+    }
+
+    if (am_hal_gpio_input_read(pin) != state)
+    {
+        if (xTaskNotifyWait(0, 1, NULL, pdMS_TO_TICKS(timeout / 1000)) == pdFALSE)
+        {
+            am_hal_gpio_interrupt_disable(psGpioMask);
+            am_hal_gpio_interrupt_clear(psGpioMask);
+            am_hal_gpio_pinconfig(pin, g_AM_HAL_GPIO_DISABLE);
+            return 0;
+        }
+    }
+
+    tick_start = am_hal_stimer_counter_get();
+    tick_end = tick_start + tick_timeout;
+    if (am_hal_gpio_input_read(pin) == state)
+    {
+        if (xTaskNotifyWait(0, 1, NULL, pdMS_TO_TICKS(timeout / 1000)) == pdFALSE)
+        {
+            am_hal_gpio_interrupt_disable(psGpioMask);
+            am_hal_gpio_interrupt_clear(psGpioMask);
+            am_hal_gpio_pinconfig(pin, g_AM_HAL_GPIO_DISABLE);
+            return 0;
+        }
+    }
+    tick_stop = am_hal_stimer_counter_get();
+    am_hal_gpio_interrupt_disable(psGpioMask);
+    am_hal_gpio_interrupt_clear(psGpioMask);
+    am_hal_gpio_pinconfig(pin, g_AM_HAL_GPIO_DISABLE);
+
+    duration_us = (tick_stop - tick_start) * 1000000 / cpu_frequency;
+
     return duration_us;
 }
