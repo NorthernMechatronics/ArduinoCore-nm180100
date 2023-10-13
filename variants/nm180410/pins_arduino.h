58c9d93caeb67ba723b8b774ed41a6a1805496cd
Joshua Wong <joshua@northernmechatronics.com>
2023-10-13 17:23:14 -0400

    Added Arduino pin definition

--- variants/nm180410/pins_arduino.h
+++ variants/nm180410/pins_arduino.h
@@ -32,4 +32,39 @@
 #ifndef _PINS_ARDUINO_H_
 #define _PINS_ARDUINO_H_
 
+#define SCL (8)
+#define SDA (9)
+
+#define D0  (23)
+#define D1  (22)
+#define D2  (26)
+#define D3  (49)
+#define D4  (48)
+#define D5  (17)
+#define D6  (46)
+#define D7  (45)
+#define D8  (41)
+#define D9  (4)
+#define D10 (20)
+#define D11 (7)
+#define D12 (6)
+#define D13 (5)
+#define D14 (12)
+#define D15 (3)
+#define D16 (37)
+#define D17 (38)
+#define D18 (24)
+#define D19 (21)
+#define D20 (27)
+#define D21 (25)
+
+#define A0  (13)
+#define A1  (29)
+#define A2  (11)
+#define A3  (31)
+#define A4  (32)
+#define A5  (33)
+#define A6  (34)
+#define A7  (35)
+
 #endif
