49233ba0126eaba4f170268f56ebdf259298fa19
Joshua Wong <joshua@northernmechatronics.com>
2023-12-01 14:26:26 -0500

    Updated build process

--- extras/release_package.bat
+++ extras/release_package.bat
@@ -1,10 +1,9 @@
 @echo off
-IF "%~1" == "" GOTO error
+IF "%1" == "" GOTO error
 IF "%~2" == "" GOTO error
-IF "%~3" == "" GOTO error
 
 
-SET "DESTINATION=staging/ArduinoCore-%~1"
+SET "DESTINATION=staging/ArduinoCore-nm180100"
 robocopy cores "%DESTINATION%/cores" stub.c *.h *.a /e
 robocopy "libraries" "%DESTINATION%/libraries" library.properties *.h *.a /e
 robocopy "variants" "%DESTINATION%/variants" AMA3B1KK.ld loadscript *.h *.c *.cpp *.a /e
@@ -13,12 +12,12 @@ copy boards.txt "%DESTINATION%"
 copy platform.txt "%DESTINATION%"
 copy programmers.txt "%DESTINATION%"
 
-sed -i "s/\[VERSION\]/%~2/g" "%DESTINATION%/platform.txt"
+sed -i "s/\[VERSION\]/%1/g" "%DESTINATION%/platform.txt"
 
-SET "FILENAME=%DESTINATION%-%~2.tar.bz2"
+SET "FILENAME=%DESTINATION%-%1.tar.bz2"
 for /f "delims= " %%i in ('basename %FILENAME%') do set BASENAME=%%i
 for /f "delims= " %%i in ('basename %DESTINATION%') do set BASEFOLDER=%%i
-SET "VERSION=%~2"
+SET "VERSION=%1"
 
 cd staging
 tar --exclude='*.git' --exclude='*.github' -cjhf "%BASENAME%" "%BASEFOLDER%"
@@ -26,11 +25,10 @@ for /f "delims= " %%i in ('sha256sum %BASENAME%') do set CHECKSUM=%%i
 for /f "delims= " %%i in ('wc -c %BASENAME%') do set SIZE=%%i
 cd ..
 
-SET "JSON_FILE=staging/package_%~1_index.json"
+SET "JSON_FILE=staging/package_nm180100_index.json"
 copy extras\package_index_template.json "%JSON_FILE%"
-sed -i "s/%%%%VARIANT%%%%/%~1/g" "%JSON_FILE%"
-sed -i "s/%%%%VERSION%%%%/%~2/g" "%JSON_FILE%"
-sed -i "s#%%%%URL%%%%#%~3#g" "%JSON_FILE%"
+sed -i "s/%%%%VERSION%%%%/%1/g" "%JSON_FILE%"
+sed -i "s#%%%%URL%%%%#%~2#g" "%JSON_FILE%"
 sed -i "s/%%%%ARCHIVENAME%%%%/%BASENAME%/g" "%JSON_FILE%"
 sed -i "s/%%%%CHECKSUM%%%%/%CHECKSUM%/g" "%JSON_FILE%"
 sed -i "s/%%%%SIZE%%%%/%SIZE%/g" "%JSON_FILE%"
@@ -38,10 +36,7 @@ sed -i "s/%%%%SIZE%%%%/%SIZE%/g" "%JSON_FILE%"
 GOTO end
 
 :error
-echo usage: release_package variant version url
-echo.
-echo valid variants are:
-echo     nm180100evb
+echo usage: release_package version url
 echo.
 echo.
 
