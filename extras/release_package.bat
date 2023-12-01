56320fa3aca5e62791852419409356598fac22ab
Joshua Wong <joshua@northernmechatronics.com>
2023-12-01 16:50:56 -0500

    build all variants

--- extras/release_package.bat
+++ extras/release_package.bat
@@ -1,5 +1,5 @@
 @echo off
-IF "%1" == "" GOTO error
+IF "%~1" == "" GOTO error
 IF "%~2" == "" GOTO error
 
 
@@ -12,12 +12,12 @@ copy boards.txt "%DESTINATION%"
 copy platform.txt "%DESTINATION%"
 copy programmers.txt "%DESTINATION%"
 
-sed -i "s/\[VERSION\]/%1/g" "%DESTINATION%/platform.txt"
+sed -i "s/\[VERSION\]/%~1/g" "%DESTINATION%/platform.txt"
 
-SET "FILENAME=%DESTINATION%-%1.tar.bz2"
+SET "FILENAME=%DESTINATION%-%~1.tar.bz2"
 for /f "delims= " %%i in ('basename %FILENAME%') do set BASENAME=%%i
 for /f "delims= " %%i in ('basename %DESTINATION%') do set BASEFOLDER=%%i
-SET "VERSION=%1"
+SET "VERSION=%~1"
 
 cd staging
 tar --exclude='*.git' --exclude='*.github' -cjhf "%BASENAME%" "%BASEFOLDER%"
@@ -27,7 +27,7 @@ cd ..
 
 SET "JSON_FILE=staging/package_nm180100_index.json"
 copy extras\package_index_template.json "%JSON_FILE%"
-sed -i "s/%%%%VERSION%%%%/%1/g" "%JSON_FILE%"
+sed -i "s/%%%%VERSION%%%%/%~1/g" "%JSON_FILE%"
 sed -i "s#%%%%URL%%%%#%~2#g" "%JSON_FILE%"
 sed -i "s/%%%%ARCHIVENAME%%%%/%BASENAME%/g" "%JSON_FILE%"
 sed -i "s/%%%%CHECKSUM%%%%/%CHECKSUM%/g" "%JSON_FILE%"
